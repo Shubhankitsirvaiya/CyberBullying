c8ebd461185986b31ba2bb3d3110a6da30a56067
Shubhankit Sirvaiya <66765582+Shubhankitsirvaiya@users.noreply.github.com>
2023-05-22 12:54:08 +0530

    testing

--- cyberbullying/cyberbullying/settings.py
+++ cyberbullying/cyberbullying/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(k%^4ber#dm7dbd)+3z%bkpve_j(jv%8avowz0+*(95)ou)xw)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
